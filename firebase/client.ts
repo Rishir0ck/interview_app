f07d26047c3b567c8d6725387e4cfa9310155737
Rishi Prajapati <rhp1508@gmail.com>
2025-05-04 18:13:45 +0530

    changes in client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -5,24 +5,24 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
 // const firebaseConfig = {
-//   apiKey: "AIzaSyCJUlzY5pAcBEjK_9sMH61ZwJXnPLllpBk",
-//   authDomain: "aceready.firebaseapp.com",
-//   projectId: "aceready",
-//   storageBucket: "aceready.firebasestorage.app",
-//   messagingSenderId: "805312363119",
-//   appId: "1:805312363119:web:e47c24da2343211e0bab9a",
-//   measurementId: "G-1N9949PJD2"
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 // };
+const firebaseConfig = {
+  apiKey: "AIzaSyCJUlzY5pAcBEjK_9sMH61ZwJXnPLllpBk",
+  authDomain: "aceready.firebaseapp.com",
+  projectId: "aceready",
+  storageBucket: "aceready.firebasestorage.app",
+  messagingSenderId: "805312363119",
+  appId: "1:805312363119:web:e47c24da2343211e0bab9a",
+  measurementId: "G-1N9949PJD2"
+};
 
 // Initialize Firebase
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
